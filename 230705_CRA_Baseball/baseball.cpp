173cbd70aa3728c6a2782c17e14981681b0caa6a
ck412.park <pigun412@gmail.com>
2023-07-05 17:13:58 +0900

    STEP 3

--- 230705_CRA_Baseball/baseball.cpp
+++ 230705_CRA_Baseball/baseball.cpp
@@ -7,18 +7,38 @@ class Baseball
 public:
 	Baseball();
 	~Baseball();
-	void guess(const string& string)
+
+	
+
+
+
+	void guess(const string& guessNumber)
 	{
-		if (string.length() != 3)
-			throw length_error("Must be three letters");
-		for (char ch : string)
-		{
-			if (ch < '0' || ch > '9')
-				throw invalid_argument("Must be number");
-		}
+		assertIllegalArgument(guessNumber);
 	}
 
 private:
+	bool isDuplicatedNumber(const string& guessNumber)
+	{
+		bool isResult = false;
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2])
+			isResult = true;
+		return true;
+	}
+	void assertIllegalArgument(const string& guessNumber)
+	{
+		if (guessNumber.length() != 3)
+			throw length_error("Must be three letters");
+		for (char ch : guessNumber)
+		{
+			if (ch >= '0' && ch <= '9')
+				throw invalid_argument("Must be number");
+		}
+		if (isDuplicatedNumber(guessNumber))
+			throw invalid_argument("Must not have the same number");
+	}
 
 };
 
