899246e56b58b34094e0ee18c4858b067bd4a01d
ck412.park <pigun412@gmail.com>
2023-07-05 17:07:30 +0900

    STEP 2

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,24 @@
 #include "pch.h"
 #include "../230705_CRA_Baseball/baseball.cpp"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLenghtIsUnmached) {
-  Baseball game;
-  EXPECT_THROW(game.guess(string("12")),length_error);
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game;
+	void assertIllegalArgument(string guessNumber) {
+		//game.guess() 수행 후 Exception이 발행해야 pass
+		try
+		{
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e)
+		{
+			//pass
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
