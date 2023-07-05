f9e59825ae3f14e5a2a2275d56241c8bf6e04ad7
ck412.park <pigun412@gmail.com>
2023-07-05 16:58:05 +0900

    STEP 1

--- 230705_CRA_Baseball/baseball.cpp
+++ 230705_CRA_Baseball/baseball.cpp
@@ -1,8 +1,17 @@
+#include <string>
+#include <stdexcept>
+using namespace std;
+
 class Baseball
 {
 public:
 	Baseball();
 	~Baseball();
+	void guess(const string& string)
+	{
+		if (string.length() != 3)
+			throw length_error("Must be three letters");
+	}
 
 private:
 
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "../230705_CRA_Baseball/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLenghtIsUnmached) {
+  Baseball game;
+  EXPECT_THROW(game.guess(string("12")),length_error);
 }
